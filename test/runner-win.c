42d23f2244086ef50ab251f95aa7ff9e88d0cd79
Jameson Nash <vtjnash@gmail.com>
2025-07-10 14:18:34 -0400

    test: simplify ASSERT_OK macro for static analysis
    I was testing a static analyzer on libuv's code, and it could not
    understand the use of a constant variable in the condition as an assert
    branch. This simplifies the code for easier static analysis. I also
    removed the explicit casts, relying instead on C's casting rules to
    catch some misuse.

--- test/runner-win.c
+++ test/runner-win.c
@@ -327,7 +327,7 @@ static int clear_line(void) {
 }
 
 
-void rewind_cursor() {
+void rewind_cursor(void) {
   if (clear_line() == -1) {
     /* If clear_line fails (stdout is not a console), print a newline. */
     fprintf(stderr, "\n");
